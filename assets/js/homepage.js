138e501a769a7885dd784aed8432b8a0062cacc0
DukeHdez <briayan27@outlook.com>
2022-04-23 17:14:49 -0400

    Add ability to request repos by user inputs

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,12 +1,32 @@
-var getUserRepos = function(user) {
-    // format the github api url
-    var apiUrl = "https://api.github.com/users/" + user + "/repos";
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
 
-    // make a request to the url
-    fetch(apiUrl).then(function(response) {
-        response.json().then(function(data) {
-          console.log(data);
-        });
-      });
-    }
-getUserRepos();
+var getUserRepos = function (user) {
+  // format the github api url
+  var apiUrl = "https://api.github.com/users/" + user + "/repos";
+
+  // make a request to the url
+  fetch(apiUrl).then(function (response) {
+    response.json().then(function (data) {
+      console.log(data);
+    });
+  });
+}
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  console.log(event);
+
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  }
+  else {
+    alert("Please enter a GitHub username");
+  }
+}
+
+userFormEl.addEventListener("submit", formSubmitHandler);
