d1b7be62b6f93e961b88e75a55500548758437bf
DukeHdez <briayan27@outlook.com>
2022-05-04 14:31:38 -0400

    Read and Use Data from query selector plus handling errors

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,8 +1,11 @@
 var issueContainerEl = document.querySelector("#issues-container");
 var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    //make a get request to url
     fetch(apiUrl).then(function (response) {
         // request was successful
         if (response.ok) {
@@ -16,13 +19,29 @@ var getRepoIssues = function (repo) {
                 }
             });
         }
+        // if not successful, redirect to homepage
         else {
-            alert("There was a problem with your request!");
+            document.location.replace("./index.html")
         }
     });
 
 }
+var getRepoName = function() {
 
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+
+    if(repoName) {
+        //display repo name on the page
+        repoNameEl.textContent = repoName;
+    getRepoIssues(repoName);
+    }
+    else {
+        //if no repo was given, redirect to the homepage
+        document.location.replace("./index.html")
+    }
+}
 var displayIssues = function (issues) {
     if (issues.length === 0) {
         issueContainerEl.textContent = "This repo has no open issues!";
@@ -71,4 +90,5 @@ var displayWarning = function(repo) {
     // append to warning container
     limitWarningEl.appendChild(linkEl)
 }
-getRepoIssues("facebook/react");
+
+getRepoName();
