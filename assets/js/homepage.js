b05682636fff086ac665014991e211aa3a696c11
DukeHdez <briayan27@outlook.com>
2022-05-04 20:44:54 -0400

    Add language buttons, call the api when buttons are clicked

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -2,6 +2,7 @@ var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
 var repoSearchTerm = document.querySelector("#repo-search-term");
+var languageButtonsEl = document.querySelector("#language-buttons")
 
 var getUserRepos = function (user) {
   // format the github api url
@@ -18,10 +19,10 @@ var getUserRepos = function (user) {
       alert("Error: GitHub User Not Found");
     }
   })
-  .catch(function(error) {
-    //Notice this '.catch()' getting chained onto the end of the '.then()' method
-    alert("Unable to connect to GitHub");
-  });
+    .catch(function (error) {
+      //Notice this '.catch()' getting chained onto the end of the '.then()' method
+      alert("Unable to connect to GitHub");
+    });
 }
 
 var formSubmitHandler = function (event) {
@@ -41,7 +42,7 @@ var formSubmitHandler = function (event) {
 }
 
 var displayRepos = function (repos, searchTerm) {
-  
+
   //check if api returned any repos
   if (repos.length === 0) {
     repoContainerEl.textContent = "No repositories found."
@@ -91,4 +92,33 @@ var displayRepos = function (repos, searchTerm) {
     repoContainerEl.appendChild(repoEl);
   }
 };
+
+var getFeaturedRepos = function (language) {
+
+  var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+
+  fetch(apiUrl)
+    .then(function (response) {
+      if (response.ok) {
+        response.json().then(function (data) {
+          displayRepos(data.items, language);
+        });
+      }
+      else {
+        alert("Error: Github User Not Found")
+      }
+    });
+};
+
+var buttonClickHandler = function(event) {
+  var language = event.target.getAttribute("data-language");
+  console.log(language);
+  if(language) {
+  getFeaturedRepos(language);
+
+  //clear old content
+  repoContainerEl.textContent = "";
+}
+
 userFormEl.addEventListener("submit", formSubmitHandler);
+languageButtonsEl.addEventListener("click", buttonClickHandler);
